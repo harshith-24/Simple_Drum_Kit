d4bffe284a76721bf328aa4fd65abfeb1ebfa5a0
harshith-24 <132118927+harshith-24@users.noreply.github.com>
2025-07-10 21:22:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var l=document.querySelectorAll(".drum").length;
+for(var i=0;i<l;i++){
+   document.querySelectorAll(".drum")[i].addEventListener("click", function()
+   {
+    var a=this.innerHTML;
+    sound(a);   
+    animation(a);
+   });
+}
+document.addEventListener("keypress",function(event)
+{
+    sound(event.key);
+    animation(event.key);
+});
+function sound(key)
+{
+    switch(key)
+    {
+        case "w":
+            var w=new Audio("./sounds/crash.mp3");
+            w.play();
+            break;
+        case "a":
+            var aa=new Audio("./sounds/kick-bass.mp3");
+            aa.play();
+            break;
+        case "s":
+            var s=new Audio("./sounds/snare.mp3");
+            s.play();
+            break;
+        case "d":
+            var d=new Audio("./sounds/tom-1.mp3");
+            d.play();
+            break;
+        case "j":
+            var j=new Audio("./sounds/tom-2.mp3");
+            j.play();
+            break;
+        case "k":
+            var k=new Audio("./sounds/tom-3.mp3");
+            k.play();
+            break;
+        case "l":
+            var l=new Audio("./sounds/tom-4.mp3");
+            l.play();
+            break;
+        default:
+            console.log("invalid entry....");
+    }        
+            
+}
+function animation(current)
+{
+    var active=document.querySelector("."+current);
+    active.classList.add("pressed");
+    setTimeout(function()
+{
+    active.classList.remove("pressed");
+},100);
+}
